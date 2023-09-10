3d11d2e799c345b7f5b4cdbf070ff25833776789
Saurabh Kapoor <saurabhkapoor@ttnpl-7294.local>
2023-09-10 18:05:30 +0530

    First file for geekCombat

---
+++ FirstFile/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Define two numbers
+        double num1 = 5.5;
+        double num2 = 10.0;
+
+        // Calculate the sum
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
