35abe0b2841bd8b80841fd71289065ec6f1f3f86
Saurabh Kapoor <saurabhkapoor@ttnpl-7294.local>
2023-09-21 00:30:34 +0530

    added 3 variable

--- FirstFile/src/Main.java
+++ FirstFile/src/Main.java
@@ -5,11 +5,12 @@ public class Main {
         // Define two numbers
         double num1 = 5.5;
         double num2 = 10.0;
+        double num3 = 10.0;
 
         // Calculate the sum
-        double sum = num1 + num2;
+        double sum = num1 + num2+ num3;
 
         // Display the result
-        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("Sum of " + num1 + " and " + num2 +"and"+num3 +" is: " + sum);
     }
 }
